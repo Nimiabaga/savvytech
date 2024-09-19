ba8d3fbe1bf60341af71ebc54a718ab21eb32102
nimiabaga <nimiabaga@yahoo.com>
2024-09-19 13:30:04 +0100

    doaminname update

--- savvytech_frontend/src/api.js
+++ savvytech_frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const ServerUrl =  "https://savvytech-eta.vercel.app:5000/api";
+const ServerUrl = "http://172.20.10.2:5000/api";
 
 // Create separate Axios instances with different base URLs
 const globalApi = axios.create({
